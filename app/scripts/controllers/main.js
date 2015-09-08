8082ed397c0cb038164f3c6e6502467bc8e44039
kdichev <konstantin.dichev@gmail.com>
2015-09-08 18:08:13 +0200

    timeline update test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,4 +8,34 @@
  */
 angular.module('sbAdminApp')
   .controller('MainCtrl', function($scope,$position) {
+  	var a = {
+  			items: [
+  			{
+  				id: 1,
+  				title: "Title 1",
+  				body: "Body 1"
+  			},
+  			{
+  				id: 2,
+  				title: "Title 2",
+  				body: "Body 2"
+  			},
+
+  			{
+  				id: 3,
+  				title: "Title 3",
+  				body: "Body 3"
+  			},
+
+  			{
+  				id: 4,
+  				title: "Title 4",
+  				body: "Body 4"
+  			}
+  	]};
+
+  	$scope.items = a;
+  	$scope.$apply();
+  	// console.log($scope.items);
+  	// console.log(a);
   });
